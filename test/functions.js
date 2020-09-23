b46d65f059e18274bd65e3759a4d4973d264e369
Ludwig Magnusson <ludwig@mediatool.com>
2020-09-23 13:00:52 +0200

    Add some simple functions and tests

--- test/functions.js
+++ test/functions.js
@@ -1,7 +1,42 @@
-import functions from '../lib/functions'
+import { assert } from 'chai'
+import {
+  add,
+  filter,
+  isEven,
+  map,
+} from '../lib/functions'
+
+const { deepEqual } = assert
 
 describe('functions', () => {
-  it('works', () => {
-    functions()
+  const numbers = [ 1, 2, 3, 4, 5 ]
+  const inc = add(1)
+
+  describe('map', () => {
+    const expected = [ 2, 3, 4, 5, 6 ]
+    it('works with all arguments', () => {
+      const result = map(inc, numbers)
+      deepEqual(expected, result)
+    })
+
+    it('works by applying partial arguments', () => {
+      const incAll = map(inc)
+      const result = incAll(numbers)
+      deepEqual(expected, result)
+    })
+  })
+
+  describe('filter', () => {
+    const expected = [ 2, 4 ]
+    it('works with all arguments', () => {
+      const result = filter(isEven, numbers)
+      deepEqual(expected, result)
+    })
+
+    it('works by applying partial arguments', () => {
+      const filterEven = filter(isEven)
+      const result = filterEven(numbers)
+      deepEqual(expected, result)
+    })
   })
 })
